2ac59dcdd8b325cc43fd9299c917fba132182d46
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-03 09:13:21 -0400

    Add activity main xml file

--- app/src/main/java/br/com/orgs/MainActivity.kt
+++ app/src/main/java/br/com/orgs/MainActivity.kt
@@ -2,12 +2,11 @@ package br.com.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Hello World!", Toast.LENGTH_LONG).show()
+        setContentView(R.layout.activity_main)
     }
 }
