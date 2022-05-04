9df3167d159fa72b114b700315ad450ba091b57d
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-04 09:22:13 -0400

    Add a products list to recyclerview adapter

--- app/src/main/java/br/com/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/orgs/ui/activity/MainActivity.kt
@@ -2,8 +2,10 @@ package br.com.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.orgs.R
+import br.com.orgs.model.Produto
 import br.com.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 
 class MainActivity : Activity() {
@@ -13,6 +15,27 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutosAdapter()
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+            Produto(
+                nome = "Maça",
+                descricao = "Fruta",
+                valor = 1.99.toBigDecimal()
+            ),
+            Produto(
+                nome = "Banana",
+                descricao = "Fruta",
+                valor = 2.0.toBigDecimal()
+            ),
+            Produto(
+                nome = "Mamão",
+                descricao = "Fruta",
+                valor = 6.5.toBigDecimal()
+            ),
+            Produto(
+                nome = "Abacate",
+                descricao = "Fruta",
+                valor = 12.0.toBigDecimal()
+            )
+        ))
     }
 }
