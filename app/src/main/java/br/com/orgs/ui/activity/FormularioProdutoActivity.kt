f28a7e2545b00a038a204bdd1bdc2dcab0d7cb6b
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-04 10:03:52 -0400

    Create FormularioProdutoActivity

---
+++ app/src/main/java/br/com/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,14 @@
+package br.com.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_produto)
+    }
+
+}
