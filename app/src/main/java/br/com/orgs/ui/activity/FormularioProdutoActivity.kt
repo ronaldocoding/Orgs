52a6672cb669ad359ebfe10e8be9f8b8dc909592
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-04 12:02:03 -0400

    Add action when clicking save product button

--- app/src/main/java/br/com/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,13 +2,41 @@ package br.com.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import br.com.orgs.R
+import br.com.orgs.model.Produto
+import java.math.BigDecimal
 
-class FormularioProdutoActivity : AppCompatActivity() {
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario_produto)
+        val botao_salvar = findViewById<Button>(R.id.salvar)
+
+        botao_salvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+            val campoValor = findViewById<EditText>(R.id.valor)
+            val valorEmTexto = campoValor.text.toString()
+
+            val valor = if (valorEmTexto.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(valorEmTexto)
+            }
+
+            val novoProduto = Produto(
+                nome = nome,
+                descricao = descricao,
+                valor = valor
+            )
+
+            Log.i("FormularioProduto", "onCreate: $novoProduto")
+        }
     }
 
 }
