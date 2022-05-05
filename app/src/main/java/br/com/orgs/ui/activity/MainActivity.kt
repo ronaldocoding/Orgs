6cd1cc40857b3b7cacc716e959c7e7a6eccc5ad0
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-05 08:04:54 -0400

    Override onResume() function to persist data in MainActivity

--- app/src/main/java/br/com/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/orgs/ui/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.orgs.R
+import br.com.orgs.dao.ProdutosDAO
 import br.com.orgs.model.Produto
 import br.com.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -14,33 +15,19 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+    }
+
+    override fun onResume() {
+        super.onResume()
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
-            Produto(
-                nome = "Maça",
-                descricao = "Fruta",
-                valor = 1.99.toBigDecimal()
-            ),
-            Produto(
-                nome = "Banana",
-                descricao = "Fruta",
-                valor = 2.0.toBigDecimal()
-            ),
-            Produto(
-                nome = "Mamão",
-                descricao = "Fruta",
-                valor = 6.5.toBigDecimal()
-            ),
-            Produto(
-                nome = "Abacate",
-                descricao = "Fruta",
-                valor = 12.0.toBigDecimal()
-            )
-        ))
+        val dao = ProdutosDAO()
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = dao.buscaTodos())
         val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
         fab.setOnClickListener {
             val intent = Intent(this, FormularioProdutoActivity::class.java)
             startActivity(intent)
         }
     }
+
 }
