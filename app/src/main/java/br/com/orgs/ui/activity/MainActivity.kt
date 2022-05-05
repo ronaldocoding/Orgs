a2b3852d8b64efac7221392132bcdbf36ebf30ab
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-05-05 07:14:59 -0400

    Add screen-forward action when clicking floating action button

--- app/src/main/java/br/com/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/orgs/ui/activity/MainActivity.kt
@@ -1,11 +1,13 @@
 package br.com.orgs.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.orgs.R
 import br.com.orgs.model.Produto
 import br.com.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -35,5 +37,10 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 valor = 12.0.toBigDecimal()
             )
         ))
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
